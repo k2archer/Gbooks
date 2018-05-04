670cd7586084add0f3c5b724868cea8c45c13fb9
pk.wei <pirate.gmail.com>
2018-05-04 13:54:34 +0800

    实现我的收藏与标题名称同步

--- app/src/main/java/kpractice/example/com/gbooks/Fragments/FavoritesFragment.java
+++ app/src/main/java/kpractice/example/com/gbooks/Fragments/FavoritesFragment.java
@@ -7,6 +7,9 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 
 import kpractice.example.com.gbooks.R;
 
@@ -19,6 +22,18 @@ public class FavoritesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_favorites, container, false);
 
+        ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+        TextView textView = (TextView) actionBar.getCustomView().findViewById(R.id.tv_title);
+        textView.setText(R.string.menu_me_favorite);
+
         return rootView;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+        TextView textView = (TextView) actionBar.getCustomView().findViewById(R.id.tv_title);
+        textView.setText(R.string.menu_me_bt_title);
     }
 }
