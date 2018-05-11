22494a6f336f2ba282603fe673a0a1bea1bc915c
pk.wei <pirate.gmail.com>
2018-05-12 03:21:08 +0800

    添加设置菜单

--- app/src/main/java/kpractice/example/com/gbooks/LoginActivity.java
+++ app/src/main/java/kpractice/example/com/gbooks/LoginActivity.java
@@ -3,6 +3,8 @@ package kpractice.example.com.gbooks;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -41,5 +43,33 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 }
                 break;
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0, 1, 1, R.string.menu_item_setting);
+        menu.add(0, 2, 1, R.string.menu_item_about);
+        menu.add(0, 3, 1, R.string.menu_item_exit);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case 1:
+                Intent i = new Intent(this, SettingsActivity.class);
+                startActivity(i);
+                break;
+            case 2:
+                i = new Intent(this, AboutActivity.class);
+                startActivity(i);
+                break;
+            case 3:
+                finish();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
